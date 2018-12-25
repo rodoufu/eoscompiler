4beb7f2b32335feb928910638d074da1db12e81c
Rodolfo Araujo <rodoufu@gmail.com>
2018-12-25 17:22:48 -0200

    TODO: Remove unused files

--- public/js/core.js
+++ public/js/core.js
@@ -1,38 +1,19 @@
 const eosCompiler = angular.module('eosCompiler', []);
 
 function mainController($scope, $http) {
+    $scope.contractsMap = {
+        0: 'Hello World',
+        1: 'AddressBook',
+        2: 'AddressBook inline action'
+    };
     $scope.formData = {};
 
-    $http.get('/api/todos')
-        .success((data) => {
-            $scope.todos = data;
-            console.log(data);
-        })
-        .error((data) => {
-            console.log('Error: ' + data);
-        });
-
-    $scope.createTodo = () => {
-        $http.post('/api/todos', $scope.formData)
-            .success((data) => {
-                $scope.formData = {};
-                $scope.todos = data;
-                console.log(data);
-            })
-            .error((data) => {
-                console.log('Error: ' + data);
+    $scope.getContractText = (id) => {
+        if (!isNaN(id)) {
+            $.get('/api/contract/' + id, (data) => {
+                editor.setValue(atob(data), -1);
             });
-    };
-
-    $scope.deleteTodo = (id) => {
-        $http.delete('/api/todos/' + id)
-            .success((data) => {
-                $scope.todos = data;
-                console.log(data);
-            })
-            .error((data) => {
-                console.log('Error: ' + data);
-            });
+        }
     };
 
     $scope.compileCode = () => {
@@ -57,5 +38,28 @@ function mainController($scope, $http) {
             .error((data) => {
                 console.log('Error: ' + data);
             });
+    };
+
+    $scope.downloadText = (filename, text, filetype) => {
+        let aTempLink = window.document.createElement('a');
+        aTempLink.href = window.URL.createObjectURL(new Blob([text], {type: filetype ? filetype : 'text/plain'}));
+        aTempLink.download = filename;
+
+        document.body.appendChild(aTempLink);
+        aTempLink.click();
+
+        document.body.removeChild(aTempLink);
+    };
+
+    $scope.downloadCode = () => {
+        $scope.downloadText('smartcontract.cpp', editor.getValue());
+    };
+
+    $scope.downloadAbi = () => {
+        $scope.downloadText('smartcontract.abi', jQuery('#abi').val());
+    };
+
+    $scope.downloadWasm = () => {
+        $scope.downloadText('smartcontract.wasm', jQuery('#wasm').val(), 'application/octet-stream');
     };
 }
--- public/js/page.js
+++ public/js/page.js
@@ -1,13 +1,13 @@
 const editor = ace.edit("editor");
-editor.setTheme("ace/theme/monokai");
+editor.setTheme("ace/theme/textmate");
 editor.session.setMode("ace/mode/c_cpp");
 
 $(document).ready(() => {
     $.get('/api/contract/0', (data) => {
-        editor.setValue(atob(data));
+        editor.setValue(atob(data), -1);
     });
 
     $('pre code').each((i, block) => {
-        hljs.highlightBlock(block);
+        // hljs.highlightBlock(block);
     });
 });
