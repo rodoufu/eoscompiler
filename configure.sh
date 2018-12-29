a2bd8db2c3ebf76149b183311f8bbe4f43f06d1e
Rodolfo Araujo <rodoufu@gmail.com>
2018-12-29 15:32:35 -0200

    Docker compose for creating the services.

--- configure.sh
+++ configure.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
+
 eoscompiler_version='0.0.1'
 
-docker build -t eoscompiler:${eoscompiler_version} eosmachine
+docker build -t eoscompiler:${eoscompiler_version} eoscompiler
 docker build -t eosnode:${eoscompiler_version} eosnode
+
+docker-compose up
+npm install
