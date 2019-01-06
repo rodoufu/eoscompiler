1efd1dc550ff1decc55bf580f813a0cf7f64e5af
Rodolfo Araujo <rodoufu@gmail.com>
2019-01-06 14:33:41 -0200

    Removing unused dependencies.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 // var mongoose = require('mongoose');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
@@ -9,7 +9,7 @@ const methodOverride = require('method-override');
 // mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');
 
 app.use(express.static(`${__dirname}/public`));
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended': 'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
