f6636cf93b58264924d49eddd65b84622d74e3da
Rodolfo Araujo <rodoufu@gmail.com>
2019-01-06 12:09:10 -0200

    EOS node is able to get data from host at http://localhost:8888/v1/chain/get_info

---
+++ eosnode/startnode.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+nodeos -e -p eosio \
+    --plugin eosio::producer_plugin \
+    --plugin eosio::chain_api_plugin \
+    --plugin eosio::http_plugin \
+    --plugin eosio::history_plugin \
+    --plugin eosio::history_api_plugin \
+    --data-dir CONTRACTS_DIR/eosio/data \
+    --config-dir CONTRACTS_DIR/eosio/config \
+    --access-control-allow-origin='*' \
+    --contracts-console \
+    --http-validate-host=false \
+    --verbose-http-errors \
+    --filter-on='*' >> nodeos.log 2>&1
