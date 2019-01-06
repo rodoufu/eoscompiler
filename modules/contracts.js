cfa37fd807d9f9851949f40147c1d972d8b056ab
Rodolfo Araujo <rodoufu@gmail.com>
2019-01-06 14:34:22 -0200

    Fake code not required anymore.

--- modules/contracts.js
+++ modules/contracts.js
@@ -6,7 +6,6 @@ const contractsMap = {
     2: 'addressbook_inline_action.cpp'
 };
 const util = require('./util');
-let compileCodeMock = false;
 // EOSIO_DISPATCH( addressbook, (upsert)(erase))
 const dispatchPattern = /EOSIO_DISPATCH\(\s*(\w*)\s*,?\s*([\(\)\w]*)\)/;
 
@@ -50,40 +49,22 @@ module.exports = {
     },
     compileCode: (data, callback) => {
         let respData = {error: null, stderr: null, stdout: null, abi: null};
-
-        if (compileCodeMock) {
-            switch (Math.floor(Math.random() * 10) % 5) {
-                case 4:
-                    respData.error = util.toBase64("Could not found the compiler.");
-                    break;
-                case 3:
-                    respData.stderr = util.toBase64("File not found.");
-                    respData.stdout = util.toBase64("");
-                    break;
-                default:
-                    respData.stderr = util.toBase64("");
-                    respData.stdout = util.toBase64("");
-                    respData.abi = util.toBase64('{"types": []}');
-                    break;
-            }
-        } else {
-            // 'docker run --rm -w /home/eoscompiler/ eoscompiler:0.0.1 ./compile.sh ' + util.toBase64(data)
-            const fileName = getContractName(data);
-            if (fileName != null) {
-                exec(`docker-compose run eosCompiler ./compile.sh ${fileName} ${util.toBase64(data)}`)
-                    .then((result) => {
-                        let resp = JSON.parse(util.fromBase64(result.stdout));
-                        // console.log(resp);
-                        callback(resp);
-                    })
-                    .catch((err) => {
-                        if (err) {
-                            respData.error = util.toBase64(err.message);
-                            callback(respData);
-                        }
-                        callback();
-                    });
-            }
+        // 'docker run --rm -w /home/eoscompiler/ eoscompiler:0.0.1 ./compile.sh ' + util.toBase64(data)
+        const fileName = getContractName(data);
+        if (fileName != null) {
+            exec(`docker-compose run eosCompiler ./compile.sh ${fileName} ${util.toBase64(data)}`)
+                .then((result) => {
+                    let resp = JSON.parse(util.fromBase64(result.stdout));
+                    // console.log(resp);
+                    callback(resp);
+                })
+                .catch((err) => {
+                    if (err) {
+                        respData.error = util.toBase64(err.message);
+                        callback(respData);
+                    }
+                    callback();
+                });
         }
     }
 };
