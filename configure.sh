c432f5ca2747cb3efed8d3560e47a15bacf93f2b
Rodolfo Araujo <rodoufu@gmail.com>
2019-01-06 12:54:27 -0200

    It should not start the docker containers.

--- configure.sh
+++ configure.sh
@@ -2,11 +2,10 @@
 
 #sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 #sudo chmod +x /usr/local/bin/docker-compose
+
 npm install
 
 eoscompiler_version='0.0.1'
 
 docker build -t eoscompiler:${eoscompiler_version} eoscompiler
 docker build -t eosnode:${eoscompiler_version} eosnode
-
-docker-compose up -d
